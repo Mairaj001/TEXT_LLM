a7625de08421a15339469dd3a08c0e09647640a0
Mairaj Ahmed Khoso <107746927+Mairaj001@users.noreply.github.com>
2025-11-17 17:27:22 +0500

    port update main

--- main.py
+++ main.py
@@ -141,6 +141,6 @@ async def paraphrase_article(request: ParaphraseRequest):
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.getenv("PORT", "3000"))
+    port = int(os.getenv("PORT", "5656"))
     uvicorn.run(app, host="0.0.0.0", port=port)
 
